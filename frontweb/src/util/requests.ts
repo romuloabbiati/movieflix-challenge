b75424256ab698fb24ef0492b23739fd7d995fee
romuloabbiati <rhabbiati@hotmail.com>
2024-02-13 08:23:15 +0000

    decoding the jwt token part1

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,15 +1,24 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = 'ROLE_VISITOR' | 'ROLE_MEMBER';
+
+type TokenData = {
+  exp: number;
+  user_name: string;
+  authorities: Role[];
+};
 
 type LoginResponse = {
-  access_token: string,
-  token_type: string,
-  expires_in: number,
-  scope: string,
-  userName: string,
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userName: string;
   userId: number;
-}
+};
 
 // export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 export const BASE_URL =
@@ -46,7 +55,7 @@ export const requestBackendLogin = (loginData: LoginData) => {
   });
 };
 
-export const saveAuthData = (obj : LoginResponse) => {
+export const saveAuthData = (obj: LoginResponse) => {
   localStorage.setItem(tokenKey, JSON.stringify(obj));
 };
 
@@ -56,27 +65,42 @@ export const getAuthData = () => {
 };
 
 export const requestBackend = (config: AxiosRequestConfig) => {
-  return axios({...config, baseURL: BASE_URL});
+  return axios({ ...config, baseURL: BASE_URL });
 };
 // NAO SEI SE AINDA VOU USAR A FC requestBackend lecture 09-18
 
-
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
-  //
-  return config;
-}, function (error) {
-  //
-  return Promise.reject(error);
-});
+axios.interceptors.request.use(
+  function (config) {
+    //
+    return config;
+  },
+  function (error) {
+    //
+    return Promise.reject(error);
+  }
+);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  //
-  return response;
-}, function (error) {
-  if(error.response.status === 401 || error.response.status === 403) {
-    history.push('/');
+axios.interceptors.response.use(
+  function (response) {
+    //
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401 || error.response.status === 403) {
+      history.push('/');
+    }
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
+);
+
+export const getTokenData = () : TokenData | undefined => {
+  const loginResponse = getAuthData();
+
+  try {
+    return jwtDecode(loginResponse.access_token) as TokenData;
+  } catch (error) {
+    return undefined;
+  }
+};
